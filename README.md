c652aa960e6f720ecb651018a44b96f4b74f37e2
osmeteor <meteorsoftware@qq.com>
2017-03-31 14:40:29 +0800

    update host url

---
+++ README.md
@@ -0,0 +1,18 @@
+# modify hosts
+
+modify /etc/hosts 
+
+## Usage
+
+### Execute from internet
+
+```
+sudo bash -c 'curl -sL https://raw.githubusercontent.com/OSMeteor/systools/master/hosts/modifyhosts.sh | bash'
+```
+
+### Download to local
+
+```
+sudo bash -c 'curl -sL https://raw.githubusercontent.com/OSMeteor/systools/master/hosts/modifyhosts.sh > ${LOCALHOSTPATH}'
+sudo chmod +x LOCALHOSTPATH
+```
--- hosts/modifyhosts.sh
+++ hosts/modifyhosts.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 sudo="sudo"
-URL_OLD_HOSTS=/Users/osmeteor/Desktop/softtek/docker/hosts/hosts
-URL_NEW_HOSTS=http://googlehosts-hostsfiles.stor.sinaapp.com/hosts
-$sudo echo 'begine'
-if [ `grep "http://www.findspace.name" $URL_OLD_HOSTS | wc -l` -gt 0 ]; then
-    BEGIN=$(grep -n "^#+BEGIN" $URL_OLD_HOSTS | awk -F: '{print $1}')
-    diff -Naur <(tail -n +$BEGIN $URL_OLD_HOSTS) <(curl -sL $URL_NEW_HOSTS) | patch -p1 $URL_OLD_HOSTS
-else 
-    $sudo curl -sL $URL_NEW_HOSTS >> $URL_OLD_HOSTS
-fi
+URL_OLD_HOSTS=/etc/hosts
+URL_NEW_HOSTS=https://raw.githubusercontent.com/OSMeteor/systools/master/hosts/list/hostslaod
+$sudo curl -sL $URL_NEW_HOSTS >> $URL_OLD_HOSTS
